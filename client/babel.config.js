7cc0f870024f8c8813167b5364b21009de20da7c
TymonJones <101831045+Tjonte03@users.noreply.github.com>
2024-10-09 19:42:27 -0700

    parsing issues fixed

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,4 +1,15 @@
-export const presets = [
-  '@babel/preset-env',
-  '@babel/preset-react',
-];
+// // export const presets = [
+//   '@babel/preset-env',
+//   '@babel/preset-react',
+// ];
+
+
+module.exports = {
+  presets: [
+    '@babel/preset-env', // Transpiles modern JavaScript (ES6+)
+    '@babel/preset-react', // Handles React's JSX syntax
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime' // Optional: helps with async/await and other features
+  ],
+};
